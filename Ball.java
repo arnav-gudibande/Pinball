62f1dd1240f81356ef99aa537229503f6209eda6
Arnav Gudibande <arnav.gudibande@gmail.com>
2016-03-02 23:22:53 -0800

    Final Release
    “n” creates new balls + array list data structure incorporated

--- Ball.java
+++ Ball.java
@@ -16,42 +16,60 @@ import java.awt.Graphics2D;
 import java.awt.geom.*;
 import java.awt.BasicStroke;
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Random;//imports all neccessary components
 
-public class Ball extends JPanel implements ActionListener, KeyListener
+public class Ball extends JComponent implements ActionListener
 {
     // instance variables - replace the example below with your own
     public double dx=0, dy=0;
-    public double x,y;
+    public double x,y,d;
     Ellipse2D.Double ball;
-    Timer t = new Timer(5, this);
+    Color c;//instance field variables for the Ball class
 
-    public Ball(int x, int y, int dx, int dy)
+    public Ball(int x, int y, int dx, int dy, int o, int d)//the explicit parameters for the BAll constructor
     {
-        t.start();
-        addKeyListener(this);
-        setFocusable(true);
-        this.x = x;
-        this.y = y;
-        this.dx = dx;
-        this.dy = dy;
+        switch(o){//switch stamenment
+            case 1://if its the users ball - then make it obey key strokes
+                this.x = x;
+                this.y = y;//sets all the instance field variables to whatever was specified
+                this.dx = dx;
+                this.dy = dy;
+                c = setBallColor();
+                break;
+            case 2://if its a random spawned ball - then make it erratic in nature
+                Random ran = new Random();//hence the random object is constructed 
+                int n = ran.nextInt(1);
+                if(n == 0) {
+                    this.dx = -ran.nextInt(20)+1;//sets the dx and dy to random values based whatever value was chosen
+                    this.dy = -ran.nextInt(20)+1;
+                }
+                else {
+                    this.dx = ran.nextInt(20)+1;//sets the dx and dy to random values based whatever value was chosen
+                    this.dy = ran.nextInt(20)+1;
+                } 
+                break;
+        }
+        this.d = d;//sets the dimensions of the ball
     }
 
-    public void paintComponent(Graphics g)
+    public void paintComponent(Graphics g)//necessary overriden method, the result of extending JFrame
     {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-        ball = new Ellipse2D.Double(x, y, 40, 40);
-        g2.setColor(Color.BLUE);
-        g2.fill(ball);
+        Graphics2D g2 = (Graphics2D) g;//creates a new Graphics2D object, and casts it b/c u are going from higher to lower dependency
+        ball = new Ellipse2D.Double(x, y, d, d);//creates a new ball, uses the instance field variables specified by the user
+        g2.setColor(setBallColor());//sets the ball to a random color, RAINBOW 
+        g2.fill(ball);//fills the ball in the graphics2d object
     }
 
     public void actionPerformed(ActionEvent e)
     {
-        repaint();
-        x+=dx;
-        y+=dy;
-        if(y<=0) moveDown();
+        move();//when the timer calls itself, move the ball
+    }
+    
+    public void move(){
+        repaint();//repaints the frame and also adds to the speed that the ball is moving in
+        x+=dx;//speed is added to the dx
+        y+=dy;//speed is added to dy
+        if(y<=0) moveDown();//if any of the bounds are breached, then make the ball move accordingly
         if(y>=540)moveUp();
         if(x<=0) moveRight();
         if(x>=760) moveLeft();
@@ -59,48 +77,36 @@ public class Ball extends JPanel implements ActionListener, KeyListener
 
     public void moveUp()
     {
-        dy += -1;
+        dy += -1;//make the dx go down to move up in the frame
     }
 
     public void moveDown()
     {
-        dy += 1;
+        dy += 1;//to move up, vice versa
     }
 
     public void moveLeft()
     {
-        dx += -1;
+        dx += -1;//to move left, dx is subtracted from
     }
 
     public void moveRight()
     {
-        dx += 1;
+        dx += 1;//to move right, dx is added to 
     }
     
-    public void keyPressed(KeyEvent e)
-    {
-        int code = e.getKeyCode();
-        if(code == KeyEvent.VK_UP)
-        {
-            moveUp();
-        }
-
-        if(code == KeyEvent.VK_DOWN)
-        {
-            moveDown();
-        }
-
-        if(code == KeyEvent.VK_LEFT)
-        {
-            moveLeft();
-        }
-
-        if(code == KeyEvent.VK_RIGHT)
-        {
-            moveRight();
-        }
+    public Color setBallColor(){//this is just for fun...
+        ArrayList<Color> colors = new ArrayList<Color>();//creating a new arraylist of random colors
+        colors.add(Color.red);//adding a multitude of colors to this
+        colors.add(Color.yellow);
+        colors.add(Color.blue);
+        colors.add(Color.orange);
+        colors.add(Color.green);
+        colors.add(Color.magenta);
+        colors.add(Color.cyan);
+        Random beast = new Random();//creating a new random object called beast
+        int x = beast.nextInt(colors.size());//picking a color based on beast's value
+        return colors.get(x);//returning that color
     }
     
-    public void keyTyped(KeyEvent e) {}
-    public void keyReleased(KeyEvent e) {}
 }
--- Starter.java
+++ Starter.java
@@ -18,17 +18,55 @@ import java.awt.BasicStroke;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Starter
+public class Starter implements ActionListener
 {
-    
+    public static Ball s;//creates a new static object of type ball 
+    public static ArrayList<Ball> bList;//creates a new ball arraylist
     public static void main(String[] args)
     {
-        JFrame f = new JFrame();
-        f.setResizable(false);
-        f.setSize(800, 600);
-        Ball s = new Ball(400,300,0,0);
-        f.add(s);
-        f.setVisible(true);
+        JFrame f = new JFrame("Rainbow Ballz");//new jframe
+        f.setResizable(false);//cant resize the frame
+        f.setSize(800, 600);//the size of the frame
+        s = new Ball(400,300,0,0,1,40);//creating the users ball, passing values using explicit parameters
+        f.add(s);//adding the ball to the jframe
+        f.setVisible(true);//setting it visible
+        Timer t = new Timer(3, s);//refreshes every 3 ms (this will be hard on the processor)
+        bList = new ArrayList<Ball>();//creating a new arraylist of balls - keeps track of how many balls are on the jframe at one time
+        
+        class bListener implements KeyListener {//new blistener class - implements the interface keylistener, therfore it needs to override three methods
+            public void keyPressed(KeyEvent e) {
+                switch(e.getKeyCode()) {//using the getkeycode method on object e
+                    case KeyEvent.VK_UP: s.moveUp();//if up arrow key, then it moves up
+                    break;
+                    case KeyEvent.VK_DOWN: s.moveDown();//if down arrow key, then it moves down
+                    break;
+                    case KeyEvent.VK_LEFT: s.moveLeft();//if left arrow key, .....
+                    break;
+                    case KeyEvent.VK_RIGHT: s.moveRight();//,..
+                    break;
+                    case KeyEvent.VK_N: //if the n button is tapped
+                        Ball beast = new Ball(400,300,0,0,2,20); //it creates a new ball object called beast with many parameters and a size of 20
+                        bList.add(beast);
+                        f.add(beast);//adds beast to the frame
+                        f.setVisible(true);
+                        t.addActionListener(beast);//adds the actionlistener of beast to the timer
+                        break;
+                    default: break;
+                }
+            }
+            public void keyReleased(KeyEvent e){}//overriding two methods, needs to be done if you implement an interface
+            public void keyTyped(KeyEvent e){}
+        }
+        f.addKeyListener(new bListener());//creating a new keylistener and adding it to the frame
+        f.add(s);//adding the users ball to the frame
+        f.setVisible(true);//setting it to visible and starting the timer
+        t.start();// refer to above line
+    }
+    
+    public void actionPerformed(ActionEvent e) {//this method must be overriden b/c this class implements Actionlistener
+        for (Ball b : bList) {//for every ball in the arraylist, this method will move each one
+            b.move();//moves each ball
+        }
     }
 
 }
